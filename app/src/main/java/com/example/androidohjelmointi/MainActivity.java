6a2b8b7e549bc4713c05bb2ba64dbb623731c465
juhodufva <juho.dufva@student.lab.fi>
2023-03-09 17:04:02 +0200

    j

--- app/src/main/java/com/example/androidohjelmointi/MainActivity.java
+++ app/src/main/java/com/example/androidohjelmointi/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.androidohjelmointi;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button testBtn;
+    private TextView helloText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testBtn = findViewById(R.id.button_test);
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+        helloText.setText(R.string.app_name);
     }
 }
